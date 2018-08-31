c9e5d1618d119630d96d84556c88cfd4a5d11179
Philrichardson85 <philrichardson85@gmail.com>
2018-08-31 15:24:14 -0400

    my commit7

--- routes/search.js
+++ routes/search.js
@@ -133,8 +133,8 @@ router.post('/related', function (req, res, next) {
                                                                                          /////////////////////////////////////
                                                                                          // create entry
 
-                                                                                        console.log(wordEntry.rhyme.length);
-                                                                                         if (wordEntry.rhyme.length !== 0) {
+                                                                                        console.log(wordEntry.definition.length);
+                                                                                         if (wordEntry.definition.length !== 0) {
                                                                                            Words.create({word: wordEntry.word, rhyme: JSON.stringify(wordEntry.rhyme), related: JSON.stringify(wordEntry.related), definition: JSON.stringify(wordEntry.definition)}, function(err, word){
                                                                                              console.log("success related added");
                                                                                             res.json(JSON.parse(word.related));
